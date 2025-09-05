fc7088be0030f1de67253dea840a32336340541e
desyreegarcia <desyree.garcia@hotmail.com>
2025-09-05 13:33:33 -0300

    MetodoAtualizar

--- src/main/java/com/example/trabalho_backend/controller/MusicaController.java
+++ src/main/java/com/example/trabalho_backend/controller/MusicaController.java
@@ -2,10 +2,7 @@ package com.example.trabalho_backend.controller;
 
 import com.example.trabalho_backend.model.Musica;
 import com.example.trabalho_backend.repositorio.MusicaRepositorio;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -15,14 +12,32 @@ import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 @RequestMapping("musicas")
 public class MusicaController {
     private MusicaRepositorio musicaRepositorio;
+
     public MusicaController(MusicaRepositorio musicaRepositorio) {
         this.musicaRepositorio = musicaRepositorio;
     }
+
     @PostMapping
     public void salvar(@RequestBody Musica musicas){
         String id = UUID.randomUUID().toString();
         musicas.setId(id);
         musicaRepositorio.save(musicas);
         System.out.println("Musica" + musicas);
+    }
+
+    @GetMapping("/{id}")
+    public Musica obterPorId(@PathVariable("id") String id) {
+        return musicaRepositorio.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable("id") String id){
+        musicaRepositorio.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Musica musicas){
+        musicas.setId(id);
+        musicaRepositorio.save(musicas);
     }
 }
